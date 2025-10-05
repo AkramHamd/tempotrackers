da7b26a3094a30bb7742953d288c1f604b2e1e3e
AkramHamd <akramhhamdouchi@gmail.com>
2025-10-05 20:58:41 +0200

    fix: update date-fns locale import

--- apps/web/src/lib/datepicker-locale.ts
+++ apps/web/src/lib/datepicker-locale.ts
@@ -1,5 +1,5 @@
 import { registerLocale } from "react-datepicker";
-import es from 'date-fns/locale/es';
+import { es } from 'date-fns/locale';
 
 export const setupDatePickerLocale = () => {
   registerLocale('es', es);
